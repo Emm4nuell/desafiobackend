94435c493a4ad0e0425000a8c3d6e9847f63e944
Emm4nuell <eduardo.eesf@gmail.com>
2025-08-31 23:35:57 -0300

    [REFACTOR] - Modificado tipo de paramentro no findall

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -69,8 +69,8 @@ class UserController {
     try {
       const page = Number(req.query.page) || 1;
       const limit = Number(req.query.limit) || 10;
-      const name = req.query.name as string | undefined;
-      const email = req.query.email as string | undefined;
+      const name = (req.query.name as string) || "";
+      const email = (req.query.email as string) || "";
       const listusers = await this.userservice.findAll(
         name,
         email,
